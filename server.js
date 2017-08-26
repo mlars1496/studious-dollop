5ceb776c48342f42db81166f06180ec01618d1d1
mlars1496 <31367045+mlars1496@users.noreply.github.com>
2017-08-26 17:53:44 +0200

    Update server.js

--- server.js
+++ server.js
@@ -152,6 +152,21 @@ app.get('/near', function (req, res) {
 
 
 
+app.get('/pasas', function (req, res){
+     var lat = parseFloat(req.body.lat);
+    var lon = parseFloat(req.body.lon);
+	   var m = req.body.m;  
+	 var t = req.body.t;
+	  var f = req.body.f;
+     db.collection('parkpoints').insert( {'m' : m, 'f' : f, 't' : t,'pos' : [lon,lat]}, {w:1}, function(err, records){
+    if (err) { throw err; }
+    res.end('success');
+    });
+ });
+
+
+
+
 
 
 // error handling
