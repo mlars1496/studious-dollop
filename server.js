a79ec418f31f9342ebcfd58c08bee85c779db969
mlars1496 <31367045+mlars1496@users.noreply.github.com>
2017-08-27 11:16:27 +0200

    Update server.js

--- server.js
+++ server.js
@@ -75,6 +75,43 @@ var initDb = function(callback) {
 
 
 
+app.get('/', function (req, res) {
+  // try to initialize the db on every request if it's not already
+  // initialized.
+  if (!db) {
+    initDb(function(err){});
+  }
+  if (db) {
+    var col = db.collection('counts');  
+    var points = db.collection('parkpoints');
+    // Create a document with request IP and current time of request
+    col.insert({ip: req.ip, date: Date.now()});
+    col.count(function(err, count){
+       res.end('success');
+    });
+  } else {
+     res.end('success');
+  }
+});
+
+
+
+
+
+app.get('/pagecount', function (req, res) {
+  // try to initialize the db on every request if it's not already
+  // initialized.
+  if (!db) {
+    initDb(function(err){});
+  }
+  if (db) {
+    db.collection('counts').count(function(err, count ){
+      res.send('{ pageCount: ' + count + '}');
+    });
+  } else {
+    res.send('{ pageCount: -1 }');
+  }
+});
 
 
  
@@ -123,6 +160,21 @@ app.get('/ws/parks/park', function (req, res){
 
 
  
+
+
+
+
+app.get('/ws/dy', function (req, res){
+  var ObjectID = mongodb.ObjectID;
+   db.collection('parkpoints').deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+   if (err) { throw err; }
+    res.end('success');
+    });
+  };
+
+
+
+
 
 
 // error handling
